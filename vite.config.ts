31fac7209f6748669554c693f1bdbe89b13ff8d4
Nikita Nafranets <root@dimensi.dev>
2025-09-30 22:49:37 +0300

    fix: correct server port configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   plugins: [svelte()],
   server: {
-    port: 3000
+    port: 3000,
   },
-  base: process.env.NODE_ENV === 'production' ? '/ok-calls-logs-analyzer/' : '/'
 });
